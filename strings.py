75bce94a696971695c55965c6e5689b7c40cb7ca
RazelRaz <razel16dcm@gmail.com>
2023-12-18 16:03:15 +0600

    Strings in Python - in Details

---
+++ strings.py
@@ -0,0 +1,22 @@
+#11. Strings in Python - in Details 
+
+friendName = 'John'
+friendName2 = 'Mary'
+
+print('Hello',friendName,'&',friendName2)
+
+sentence = "Ron say \"He is a good Boy\""
+print(sentence)
+
+sentence2 = """John Says
+How are you
+bro"""
+print(sentence2)
+
+print(friendName[2])
+
+# for loop
+print("Lets use a for loop\n")
+
+for char in friendName2:
+  print(char)
