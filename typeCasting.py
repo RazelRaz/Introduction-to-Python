9f6c3f4a809b6c40de466d76c1c9d085402d3a95
RazelRaz <razel16dcm@gmail.com>
2023-12-18 12:35:53 +0600

    Typecasting

---
+++ typeCasting.py
@@ -0,0 +1,19 @@
+# Typecasting
+
+# explicit conversion example - self
+a = '1'
+b = '3'
+print(int(a) + int(b))
+
+# example 2
+string = "15"
+num1 = 5
+makeint = int(string)
+print(makeint + num1)
+
+
+# implicit conversion - by python
+
+c= 1.9
+d = 8
+print(c+d)
